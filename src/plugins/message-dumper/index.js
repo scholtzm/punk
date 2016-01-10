70bfe6a0030141714ed7774833ca77d12aa1b61c
Michael Scholtz <michael.scholtz@outlook.com>
2016-01-10 20:58:26 +0100

    ignore multi messages

--- src/plugins/message-dumper/index.js
+++ src/plugins/message-dumper/index.js
@@ -5,10 +5,17 @@ exports.plugin = function(API) {
   var utils = API.getUtils();
   var Steam = API.getSteam();
 
+  var ignored = {};
+  ignored[Steam.EMsg.Multi] = true;
+
   API.registerHandler({
     emitter: 'client',
     event: 'message'
   }, function(header) {
+    if(header.msg in ignored) {
+      return;
+    }
+
     log.debug('Received message %d (%s).', header.msg, utils.enumToString(header.msg, Steam.EMsg));
   });
 };
