7a941863b42cc1c5508ef264726609c774f1146e
Michael Scholtz <michael.scholtz@outlook.com>
2017-08-12 18:15:18 +0200

    fix node4  build

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { task, series, parallel } = require('gulp');
+const gulp = require('gulp');
 
 const clean = require('./tasks/clean');
 const buildJs = require('./tasks/build-js');
@@ -7,9 +7,9 @@ const copyFonts = require('./tasks/copy-fonts');
 const watch = require('./tasks/watch');
 const package = require('./tasks/package');
 
-const build = parallel(buildJs, buildSass, copyFonts);
+const build = gulp.parallel(buildJs, buildSass, copyFonts);
 
-task('clean', clean);
-task('build', series(clean, build));
-task('watch', series(clean, build, watch));
-task('package', package);
+gulp.task('clean', clean);
+gulp.task('build', gulp.series(clean, build));
+gulp.task('watch', gulp.series(clean, build, watch));
+gulp.task('package', package);
